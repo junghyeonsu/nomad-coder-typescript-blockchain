34a4e47871c882794fcb18ffbf5df1054a8d4940
junghyeonsu <jung660317@naver.com>
2021-01-07 22:56:00 +0900

    7강
    Classes on Typescript part One
    - 타입스크립트를 컴파일했을 때 인터페이스는 자바스크립트에 안 남음
    - 만약 남기고 싶다면 class를 생성해서 컴파일하면 자바스크립트 코드에 생성이 된다.
    - 클래스의 constructor는 생성자와 같다. 클래스가 실행될 때마다 실행되는 함수임

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,30 @@
-interface Human {
-    name:string,
-    age:number,
-    gender:string
-};
 
-const person = {
-    name : "정현수",
-    age : 25,
-    gender : "female"
-};
+// 인터페이스는 자바스크립트 코드로 변환이 안됨
+// interface Human {
+//     name:string,
+//     age:number,
+//     gender:string
+// };
+
+// 아주 가끔 자바스크립트로 인터페이스를 넣고싶으면, 클래스를 사용
+class Human {
+    public name     :string;
+    public age      :number;
+    public gender   :string;
+    // 생성자라고 생각하면됨, 클래스가 생성될 때 마다 실행
+    constructor(name: string, age: number, gender: string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
+const hyeonsu = new Human("hyeonsu", 25, "male")
 
 const sayHi = (person: Human) : void => {
     console.log(`Hello ${person.name}, you're age is ${person.age}, gender is ${person.gender}!`);
 };
 
-sayHi(person);
+sayHi(hyeonsu);
 
 export {};

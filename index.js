d1198e3b83313f08878f6a43545564b2d3984f3e
junghyeonsu <jung660317@naver.com>
2021-01-07 22:16:10 +0900

    5강
    Types in Typescript
    - 매개변수에 타입을 명시해서 오류가 안나도록 하는 방법
    - 함수의 리턴방식에도 명시를 할 수 있다.
    - tsc-watch 설치해서 nodemon처럼 계속 코드가 변하는지 확인해서 자동적으로 컴파일하고 실행해주는 라이브러리 설치
    - dist 폴더는 자바스크립트 파일이 저장되는 폴더
    - src 파일은 타입스크립트 소스코드가 존재하는 폴더

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var name = "Hyeon-su", age = 24, gender = "male";
-var sayHi = function (name, age, gender) {
-    console.log("Hello " + name + ", you're age is " + age + ", gender is " + gender);
-};
-sayHi(name, age, gender);
-//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name:string, age:number, gender:string) : void => {
+    console.log(`Hello ${name}, you're age is ${age}, gender is ${gender}!`);
+};
+
+sayHi("현수", 2, "male");
+
+export {};

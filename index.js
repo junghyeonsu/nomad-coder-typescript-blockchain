3237b167d97eb5b9e40f131b735a4fcc3a0d00c3
junghyeonsu <jung660317@naver.com>
2021-01-07 21:51:51 +0900

    4강
    First steps with Typescript
    - 타입스크립트의 강력함은 사용자를 멍청한 행동으로부터 지켜준다.
    - ? 기호는 선택지를 input을 넣을지 말지 선택지를 준다.
    - 자바스크립트는 모든 매개변수를 넣지않아도 실행되지만, 타입스크립트는 그렇지않다. (엄격하다)

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("Hello World");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = "Hyeon-su", age = 24, gender = "male";
+var sayHi = function (name, age, gender) {
+    console.log("Hello " + name + ", you're age is " + age + ", gender is " + gender);
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World")
+const name = "Hyeon-su",
+    age = 24,
+    gender = "male";
+
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you're age is ${age}, gender is ${gender}`);
+};
+
+// ? 는 선택을 주는거임 넣든 말든
+// const sayHi = (name, age, gender?) => {
+//     console.log(`Hello ${name}, you're age is ${age}, gender is ${gender}`);
+// };
+
+sayHi(name, age, gender);
+
+export {};
